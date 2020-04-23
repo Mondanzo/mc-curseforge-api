760ff26fc766748e3ba7b2a732748b9958189356
Mondanzo <hamburg.karl.nehring@gmail.com>
2020-04-24 01:49:24 +0200

    Rewrite for new api!
    Thanks curseforge for still not offering devs an easy way to interact
    with your api!

--- index.js
+++ index.js
@@ -4,17 +4,16 @@ const querystring = require("querystring");
 const Mod = require("./objects/Mod");
 const ModFile = require("./objects/Files");
 
-const base_url = "https://ddph1n5l22.execute-api.eu-central-1.amazonaws.com/dev/";
-
+const base_url = "https://addons-ecs.forgesvc.net/api/v2/addon/";
 
 /**
- * @description Generic Convertion Function
+ * @description Generic Conversion Function (The Epic GCF)
  * @private
  * @param {Object} object - The object
  * @returns {Object} Returns the object
  */
-function basic_convertion_function(object){
-    return object;
+function basic_conversion_function(object) {
+	return object;
 }
 
 /**
@@ -23,24 +22,29 @@ function basic_convertion_function(object){
  * @param {string} url - Url to get
  * @param {Object} options - Object to stringify to url options
  */
-function innerGet(url, options = {}, convertionFunction = basic_convertion_function){
-    return new Promise((resolve, reject) => {
-        url += "?" + querystring.stringify(options);
-        https.get(url, function(response){
-            if (response && response.statusCode === 200) {
-                let data = "";
-                response.on("data", (chunk) => {
-                    data += chunk;
-                });
+function innerGet(
+	url,
+	options = {},
+	conversionFunction = basic_conversion_function
+) {
+	return new Promise((resolve, reject) => {
+		if (Object.keys(options).length)
+			url += "&" + querystring.stringify(options);
+		https.get(url, function (response) {
+			if (response && response.statusCode === 200) {
+				let data = "";
+				response.on("data", (chunk) => {
+					data += chunk;
+				});
 
-                response.on("end", () => {
-                    resolve(convertionFunction(JSON.parse(data)));
-                });
-            } else {
-                reject(response.statusCode);
-            }
-        });
-    });
+				response.on("end", () => {
+					resolve(conversionFunction(JSON.parse(data)));
+				});
+			} else {
+				reject(response.statusCode);
+			}
+		});
+	});
 }
 
 /**
@@ -49,59 +53,75 @@ function innerGet(url, options = {}, convertionFunction = basic_convertion_funct
  * @see getMods
  */
 
+/**
+ * @description Helper Object for variations of sort options
+ * @readonly
+ * @enum {number}
+ */
+module.exports.SORT_TYPES = {
+	FEATURED: 0, // Sort by Featured
+	POPULARITY: 1, // Sort by Popularity
+	LAST_UPDATE: 2, // Sort by Last Update
+	NAME: 3, // Sort by Name
+	AUTHOR: 4, // Sort by Author
+	TOTAL_DOWNLOADS: 5, // Sort by Total Downloads
+};
 
 /**
-* @function getMods
-* 
-* @description Get an overview of all possible mods.
-* @param {Object} options - A Object containing optional parameters.
-* @param {string} options.mc_version - The Minecraft version string to use. ("1.12.2", "1.13")
-* @param {string} options.mod_name - The mod name to search for. ("immersive", "botania")
-* @param {string} options.owner - The creators name to search for. ("Vazkii", "Mondanzo")
-* @param {number} options.page_num - The page to use. (in combination with options.page_size)
-* @param {number} options.page_size - The amount of items to show per page. (in combination with options.page_num)
-* @param {function} callback - Optional callback to use instead of Promise.
-* @returns {Promise.<Mod[], Error>} A promise containing the json object returned by the Curse API on successful 200 response.
-*/
+ * @function getMods
+ *
+ * @description Get an overview of all possible mods.
+ * @param {Object} options - A Object containing optional parameters.
+ * @param {string} options.gameVersion - The Minecraft version string to use. ("1.12.2", "1.13") @see SORT_TYPES
+ * @param {string} options.searchFilter - Term to search for.
+ * @param {number} options.index - The page to use.
+ * @param {number} options.pageSize - The amount of items to show per page. (Limit 500)
+ * @param {function} callback - Optional callback to use instead of Promise.
+ * @returns {Promise.<Mod[], Error>} A promise containing the json object returned by the Curse API on successful 200 response.
+ */
 module.exports.getMods = function (options = {}, callback) {
-    if (options && typeof options === 'function') {
-        callback = options;
-        options = {};
-    }
-    let promise = innerGet(base_url + "mods", options, function(obj){
-        let mods = [];
-        for(let m of obj.result){
-            mods.push(new Mod(m));
-        }
-        return mods;
-    });
-    if (callback && typeof callback === 'function')
-        promise.then(callback.bind(null, null), callback);
-    return promise;
+	if (options && typeof options === "function") {
+		callback = options;
+		options = {};
+	}
+	let promise = innerGet(
+		base_url + "search?gameId=432&sectionId=6",
+		options,
+		function (obj) {
+			let mods = [];
+			for (let m of Object.values(obj)) {
+				mods.push(new Mod(m));
+			}
+			return mods;
+		}
+	);
+	if (callback && typeof callback === "function")
+		promise.then(callback.bind(null, null), callback);
+	return promise;
 };
 
 /**
  * @function getMod
- * 
+ *
  * @description Get information about a specific mod using the identifier.
- * @param {string|number} identifier - The mods slug or curse id to find the mod with.
+ * @param {number} identifier - The mods curse id to find the mod with.
  * @param {function} callback - Optional callback to use instead of Promise.
  * @returns {Promise.<Mod, Error>} A promise containing the json object returned by the Curse API on successful 200 response.
  */
 module.exports.getMod = function (identifier, callback) {
-    let promise = innerGet(base_url + "mod/" + identifier, {}, function(obj){
-        return new Mod(obj.result);
-    });
-    if (callback && typeof callback === 'function')
-        promise.then(callback.bind(null, null), callback);
-    return promise;
+	let promise = innerGet(base_url + identifier, {}, function (obj) {
+		return new Mod(obj);
+	});
+	if (callback && typeof callback === "function")
+		promise.then(callback.bind(null, null), callback);
+	return promise;
 };
 
 /**
  * @function getModFiles
  *
  * @description Get information about the releases of a specific mod.
- * @param {string|number} identifier - The mods slug or curse id to find the mod with.
+ * @param {number} identifier - The mods curse id to find the mod with.
  * @param {Object} options - Optional Options object to use for filtering.
  * @param {string} options.mc_version - The Minecraft version string to use. ("1.12.2", "1.13")
  * @param {string} options.channel - The channel to use. ("Beta", "Release")
@@ -109,22 +129,37 @@ module.exports.getMod = function (identifier, callback) {
  * @param {function} callback - Optional callback to use instead of Promise.
  * @returns {Promise.<ModFile[], Error>} A promise containing the json object returned by the Curse API on successful 200 response.
  */
-module.exports.getModFiles = function(identifier, options = {}, callback){
-    if (options && typeof options === 'function') {
-        callback = options; 
-        options = {};
-    }
+module.exports.getModFiles = function (identifier, options = {}, callback) {
+	if (options && typeof options === "function") {
+		callback = options;
+		options = {};
+	}
 
-    if(options.hasOwnProperty("newest_only"))
-        options.newest_only = options.newest_only ? 1 : 0;
-    let promise = innerGet(base_url + "mod/" + identifier + "/files", options, function(obj){
-        let files = [];
-        for (let f of obj.result) {
-            files.push(new ModFile(f));
-        }
-        return files;
-    });
-    if (callback && typeof callback === 'function')
-        promise.then(callback.bind(null, null), callback);
-    return promise;
+	if (options.hasOwnProperty("newest_only"))
+		options.newest_only = options.newest_only ? 1 : 0;
+	let promise = innerGet(base_url + identifier + "/files", options, function (
+		obj
+	) {
+		let files = [];
+		for (let f of Object.values(obj)) {
+			files.push(new ModFile(f));
+		}
+		return files;
+	});
+	if (callback && typeof callback === "function")
+		promise.then(callback.bind(null, null), callback);
+	return promise;
+};
+
+module.exports.getModDescription = function (identifier, callback) {
+	let promise = innerGet(
+		base_url + identifier + "/description",
+		undefined,
+		function (obj) {
+			return obj;
+		}
+	);
+	if (callback && typeof callback === "function")
+		promise.then(callback.bind(null, null), callback);
+	return promise;
 };
--- objects/Mod.js
+++ objects/Mod.js
@@ -1,47 +1,74 @@
 const curseforge = require("../index");
+const ModFile = require("../objects/Files");
 
 module.exports = class {
+	/**
+	 * @method Mod.getFiles
+	 * @description Simple function to call GetModFiles with predefined identifier.
+	 * @see CurseForgeAPI.getModFiles
+	 */
+	getFiles(options, callback) {
+		return curseforge.getModFiles(this.id, options, callback);
+	}
 
-    /**
-     * @method Mod.getFiles
-     * @description Simple function to call GetModFiles with predefined identifier.
-     * @see CurseForgeAPI.getModFiles
-     */
-    getFiles(options, callback){
-        return curseforge.getModFiles(this.id, options, callback);
-    }
+	/**
+	 * @method Mod.getDescription
+	 * @description Simple function to call GetModDescription with predefined identifier.
+	 * @see CurseForgeAPI.getModDescription
+	 */
+	getDescription(callback) {
+		return curseforge.getModDescription(this.id, callback);
+	}
 
-    /**
-     * @name Mod
-     * @class Mod
-     * @description A Mod Object representing a file of a specific mod
-     * @param {Object} mod_object - Mod object to create object from
-     * @property {number} id - The Curse Id of the mod.
-     * @property {string} key - The Curse slug of the mod.
-     * @property {string} name - The display name of the mod.
-     * @property {string} description - A detailed description of the mod in html.
-     * @property {string} url - The url to the mods page.
-     * @property {string} owner - The name of the creator.
-     * @property {string[]} categories - Array of user-defined categories for this mod.
-     * @property {string} logo - The url to the logo of the mod.
-     * @property {number} downloads - The amount of downloads of the mod.
-     * @property {timestamp} created - A timestamp of the time the mod got created.
-     * @property {timestamp} updated - A timestamp of the time the mod got updated.
-     * @property {timestamp} visited - ...A timestamp? Honestly, I don't know.
-     */
-    constructor(mod_object){
-        this.id = mod_object.id;
-        this.key = mod_object.key;
-        this.name = mod_object.name;
-        this.blurb = mod_object.blurb;
-        this.description = mod_object.description;
-        this.url = mod_object.url;
-        this.owner = mod_object.owner;
-        this.categories = mod_object.categories;
-        this.logo = mod_object.avatar;
-        this.downloads = mod_object.download_count;
-        this.created = mod_object.created;
-        this.updated = mod_object.updated;
-        this.visited = mod_object.visited;
-    }
+	/**
+	 * @name Mod
+	 * @class Mod
+	 * @description A Mod Object representing a file of a specific mod
+	 * @param {Object} mod_object - Mod object to create object from
+	 * @property {number} id - The Curse Id of the mod.
+	 * @property {string} name - The display name of the mod.
+	 * @property {object[]} authors - An array of authors names.
+	 * @property {object[]} attachments - An array of attachments objects from the description.
+	 * @property {string} url - The url to the mods page.
+	 * @property {string} summary - A short description to advertise the mod.
+	 * @property {number} defaultFileId - The default file id of the mod.
+	 * @property {number} downloads - The amount of downloads of the mod.
+	 * @property {ModFile[]} latestFiles - An array of ModFile's containing the latest files.
+	 * @property {string} key - The Curse slug of the mod.
+	 * @property {boolean} featured - Is the mod featured?
+	 * @property {number} popularityScore - Some kind of score? Not sure.
+	 * @property {number} gamePopularityRank - The rank of the mod.
+	 * @property {string} primaryLanguage - the primary language of the mod
+	 * @property {object} logo - The attachment object of the logo of the mod.
+	 * @property {timestamp} created - A timestamp of the time the mod got created.
+	 * @property {timestamp} updated - A timestamp of the time the mod got updated.
+	 * @property {timestamp} released - A timestamp of the time the mod got released.
+	 * @property {boolean} available - true if the mod is available.
+	 * @property {boolean} experimental - true if the mod is experimental.
+	 */
+	constructor(mod_object) {
+		this.id = mod_object.id;
+		this.name = mod_object.name;
+		this.authors = mod_object.authors;
+		this.attachments = mod_object.attachments;
+		this.url = mod_object.websiteUrl;
+		this.summary = mod_object.summary;
+		this.defaultFileId = mod_object.defaultFileId;
+		this.downloads = mod_object.downloadCount;
+		this.latestFiles = [];
+		for (let f of mod_object.latestFiles) {
+			this.latestFiles.push(new ModFile(f));
+		}
+		this.key = mod_object.slug;
+		this.featured = mod_object.isFeatured;
+		this.popularityScore = mod_object.popularityScore;
+		this.gamePopularityRank = mod_object.gamePopularityRank;
+		this.primaryLanguage = mod_object.primaryLanguage;
+		this.logo = this.attachments[0];
+		this.updated = mod_object.dateModified;
+		this.created = mod_object.dateCreated;
+		this.released = mod_object.dateReleased;
+		this.available = mod_object.isAvailable;
+		this.experimental = mod_object.isExperimental;
+	}
 };
